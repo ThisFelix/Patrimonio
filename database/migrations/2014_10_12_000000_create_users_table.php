1b920cadfcc0c4ce53985d83e3be2eb5ce0ea40e
vagrant <vagrant@homestead>
2018-03-17 18:53:14 +0000

    criação das migrations das tabelas rooms, patrimonios e alteração da tabela users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,10 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('prontuario')->nullable();
+            $table->string('image', 100)->nullable();
             $table->rememberToken();
-            $table->timestamps();
+
         });
     }
 
