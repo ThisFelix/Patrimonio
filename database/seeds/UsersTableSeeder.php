0f82102ce154256d95854784b7f3a98bb4b48b16
vagrant <vagrant@homestead>
2018-03-17 21:44:28 +0000

    sistema de login automático - laravel

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Matheus Félix',
+            'email' => 'mat@felix.com',
+            'password'=> bcrypt('admin'),
+            'prontuario' => 'gu1661027'
+        ]);
+    }
+}
