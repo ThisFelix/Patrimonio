b97e666036def1da2ebec85d086bf0e3c6afda84
vagrant <vagrant@homestead>
2018-04-06 02:57:45 +0000

    envio de email

--- config/mail.php
+++ config/mail.php
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('MAIL_USERNAME', 'ifsp.patrimonio@gmail.com'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('MAIL_PASSWORD', '@_Santos1996'),
 
     /*
     |--------------------------------------------------------------------------
