96c81107ab95bfd4c1b2575c40717e4237e21dea
vagrant <vagrant@homestead>
2018-04-08 03:16:49 +0000

    alterações de redefinição de senha

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',heroku_292bfe075605080
+            'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
