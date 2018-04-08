7df38ae4a2237d956853bd8e27d38e4a6bf75fbd
vagrant <vagrant@homestead>
2018-04-08 02:20:19 +0000

    alteração do login de redirecionamento de senha 9

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = redirect('/')->back()->with('referrer', true);;
 
     /**
      * Create a new controller instance.
