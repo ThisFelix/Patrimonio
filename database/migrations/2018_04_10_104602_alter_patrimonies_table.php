5bf8bcb2bab087f0546426255fee9efe91446a31
Márcio Isaque <marcio@montenegroev.com.br>
2018-04-10 14:53:08 -0300

    Alterações/Criações Tabelas
    Executar
    
    Composer Update
    
    após esse commit

---
+++ database/migrations/2018_04_10_104602_alter_patrimonies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterPatrimoniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('patrimonies', function (Blueprint $table) {
+            $table->string('patrimonyNumber')->nullable();
+            $table->string('serialNumber')->nullable();
+            $table->integer('status')->default(1)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('patrimonies', function (Blueprint $table) {
+            $table->dropColumn('patrimonyNumber');
+            $table->dropColumn('serialNumber');
+            $table->dropColumn('status');
+        });
+    }
+}
