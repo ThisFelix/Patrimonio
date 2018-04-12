2f295f4679def935bcd08ee3309a7f84c36648b2
vagrant <vagrant@homestead>
2018-04-12 03:12:06 +0000

    alterãção adminlte

--- resources/views/vendor/adminlte/login.blade.php
+++ resources/views/vendor/adminlte/login.blade.php
@@ -28,7 +28,7 @@
 
                 <div class="form-group has-feedback {{ $errors->has('email') ? 'has-error' : '' }}">
                     <input type="email" name="email" class="form-control" value="{{ old('email') }}"
-                           placeholder="{{ @lang('adminlte::adminlte.email') }}">
+                           placeholder="{{ trans('messages.email') }}">
                     <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
                     @if ($errors->has('email'))
                         <span class="help-block">
